0d13e6e73702d3bd11bf8f5d03f56de88f2a3a30
unknown <vtdanh0701@gmail.com>
2020-01-06 07:19:39 -0800

    about page styled

--- src/Component/Project/Project.js
+++ src/Component/Project/Project.js
@@ -14,26 +14,26 @@ const Project = () => {
             
             <div className='project-card-container'>
                 <ProjectCard image={images.project4} 
-                             title='Zero Bug - Bug Tracking System'
+                             title='Zero Bug'
                              stack='Full-Stack'
                              github='https://github.com/vtdanh0701/Zero-Bug'
                              live='https://zerobug.herokuapp.com/'
                              tech={MERN}
                              />
                 <ProjectCard image={images.project2} 
-                            title='iCEPI - Food Recipe App'
+                            title='iCEPI'
                             stack='Full-Stack'
                             github='https://github.com/vtdanh0701/Project3-iCepi'
                             live='https://icepi.herokuapp.com/'
                             tech={MERN}/>
                 <ProjectCard image={images.project3} 
-                            title='Scalakor - Music Theory App'
+                            title='Scalakor'
                             stack='Full-Stack'
                             github='https://github.com/vtdanh0701/GA-Project2-Scalakor'
                             live='https://scalakor.herokuapp.com/'
                             tech={['node','express','postgres']}/>
                 <ProjectCard image={images.project1} 
-                            title="Space Invader - Shoot 'em Up game"
+                            title="Space Invader"
                             stack='front-end'
                             github='https://github.com/vtdanh0701/space_invader'
                             live='https://vtdanh0701.github.io/space_invader/'
