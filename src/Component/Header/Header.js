7eb8fe2ba16ca2f3e5d6f5b4e34c724a3d7d46d2
unknown <vtdanh0701@gmail.com>
2020-01-06 08:32:55 -0800

    done basic styling, ready for phone and pc

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -6,12 +6,12 @@ import {Link} from 'react-scroll'
 const Header = ()=>{  
     return(
         <div className='page-header'>
-            <div className='header-text'>
-                <Typing>
+            
+                <Typing className='header-text'>
                     <h1>Hi,<Typing.Delay ms={500}/> I'm Dan.</h1><Typing.Delay ms={1000}/>
                     <h1>A Web Developer based in Seattle.</h1>
                 </Typing>
-            </div>
+            
             <div className='scroll-container'>
                 <Link to='project-container'
                       duration={500} 
